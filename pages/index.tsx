7ba7376d690994f3765c172f39acde1877c9ca53
Sho Fukazawa <fukazawa_shou@taito.co.jp>
2024-01-26 18:06:50 +0900

    txt変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   };
   return (
     <div className={styles.page}>
-      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+      <button onClick={handleClick} className={styles.button}>One more cat!</button>
       <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
     </div>
   );
